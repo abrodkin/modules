2248011a2bc6143840e15c509ecc3310946db1cd
rkowen <rkowen>
2002-03-10 06:08:31 +0000

    Included the Tcl break code.

--- version.c
+++ version.c
@@ -1,4 +1,4 @@
-const char *version_string = "3.2.0";
+const char *version_string = "3.2.0a";
 const char *date_string = "2001-08-08";
-/* NEW TAG "modules-3-2-0" */
-/* OLD TAG "modules-3-2-0" */
+/* NEW TAG "modules-3-2-0a" */
+/* OLD TAG "modules-3-2-0a" */
