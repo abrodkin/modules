b440dc2240798eb4103b8126846cb871f3ad5fe8
rkowen <rkowen>
2005-11-11 22:51:08 +0000

    * Updated the config/tcl.m4 (used by configure)

--- version.c
+++ version.c
@@ -1,4 +1,4 @@
-const char *version_string = "3.2.0a";
+const char *version_string = "3.2.0b";
 const char *date_string = "2005-10-31";
 /* NEW TAG "modules-3-2-0a" */
 /* OLD TAG "modules-3-1-6" */
