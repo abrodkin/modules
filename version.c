515e9af93225a0340c3535b2ac7608260d028a12
rkowen <rkowen>
2006-02-04 00:21:49 +0000

    Set to patch level 1

--- version.c
+++ version.c
@@ -1,4 +1,4 @@
-const char *version_string = "3.2.0";
-const char *date_string = "2006-01-17";
-/* NEW TAG "modules-3-2-0" */
+const char *version_string = "3.2.0p1";
+const char *date_string = "2006-02-03";
+/* NEW TAG "modules-3-2-0p1" */
 /* OLD TAG "modules-3-1-6" */
