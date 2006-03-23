104291c4698cc73ce5b7f248ea0b25e21cf1d8ff
rkowen <rkowen>
2006-03-23 23:31:53 +0000

    Fixed the csh init script.

--- version.c
+++ version.c
@@ -1,4 +1,4 @@
-const char *version_string = "3.2.0p1";
-const char *date_string = "2006-02-03";
-/* NEW TAG "modules-3-2-0p1" */
+const char *version_string = "3.2.1";
+const char *date_string = "2006-03-23";
+/* NEW TAG "modules-3-2-1" */
 /* OLD TAG "modules-3-2-0" */
