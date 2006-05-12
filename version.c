388610404a95ed5b741d0579d18989a529598c55
rkowen <rkowen>
2006-05-12 22:23:35 +0000

    Included the patch by David Golden Dias [ 1460801 ] to allow version names to be substrings of other version names.

--- version.c
+++ version.c
@@ -1,4 +1,4 @@
-const char *version_string = "3.2.2";
-const char *date_string = "2006-05-04";
-/* NEW TAG "modules-3-2-2" */
-/* OLD TAG "modules-3-2-1" */
+const char *version_string = "3.2.3";
+const char *date_string = "2006-05-12";
+/* NEW TAG "modules-3-2-3" */
+/* OLD TAG "modules-3-2-2" */
